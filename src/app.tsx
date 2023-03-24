1c4f680d3bae2333a59db98b8ffdec99addeddb3
Alex Muench <ammuench@gmail.com>
2023-03-23 23:40:18 -0500

    🎉 Initial Commit & Setup

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import * as ReactDOM from "react-dom";
+
+import "./index.css";
+
+function render() {
+  ReactDOM.render(
+    <h2 className="text-3xl font-bold underline">Hello from React!</h2>,
+    document.body
+  );
+}
+
+render();
